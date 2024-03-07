cfb9cad0bb17d3f9d732c2cc74532c2f28893882
lmarch <lmarch@yandex.ru>
2024-03-08 01:04:47 +0300

    Adding API for Reviews.

---
+++ api/src/main/java/com/lmarch/api/core/review/ReviewService.java
@@ -0,0 +1,17 @@
+package com.lmarch.api.core.review;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public interface ReviewService {
+    /**
+     * Sample usage: "curl $HOST:$PORT/review?productId=1".
+     *
+     * @param productId ID of the product
+     * @return the reviews of the product
+     */
+    @GetMapping(value = "/review", produces = "application/json")
+    List<Review> getReviews(@RequestParam(value = "productId") int productId);
+}
