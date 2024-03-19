efba9661f185759d58771c621eaad926935b5d97
lmarch <lmarch@yandex.ru>
2024-03-19 21:08:11 +0300

    Creating a MySqlTestBase class. The base class is used to run one Docker container for MySQL for each test class.

---
+++ microservices/review-service/src/test/java/com/lmarch/microservices/core/review/MySqlTestBase.java
@@ -0,0 +1,22 @@
+package com.lmarch.microservices.core.review;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
+
+public abstract class MySqlTestBase {
+    private static final JdbcDatabaseContainer database = new MySQLContainer("mysql:8.0.33")
+            .withStartupTimeoutSeconds(900);
+
+    static {
+        database.start();
+    }
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+    }
+}
