7e939191e028f43742bca9ca7e98166b53c52d05
lmarch <lmarch@yandex.ru>
2024-03-26 00:32:06 +0300

    Adding mapper classes to all microservices (product, recommendation, review).

---
+++ microservices/product-service/src/main/java/com/lmarch/microservices/core/product/services/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.lmarch.microservices.core.product.services;
+
+import com.lmarch.api.core.product.Product;
+import com.lmarch.microservices.core.product.persistence.ProductEntity;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface ProductMapper {
+
+    @Mappings({
+        @Mapping(target = "serviceAddress", ignore = true)
+    })
+    Product entityToApi(ProductEntity entity);
+
+    @Mappings({
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "version", ignore = true)
+    })
+    ProductEntity apiToEntity(Product api);
+}
