b5a90fdd9d95e08fc464fa0ec65adaf58b4db4f1
lmarch <lmarch@yandex.ru>
2024-03-25 19:57:33 +0300

    Adding @PostMapping and @DeleteMapping in the recommendation service interface.

--- api/src/main/java/com/lmarch/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/com/lmarch/api/core/recommendation/RecommendationService.java
@@ -1,13 +1,30 @@
 package com.lmarch.api.core.recommendation;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public interface RecommendationService {
+
     /**
-     * Sample usage: "curl $HOST:$PORT/recommendation?productId=1".
+     * Sample usage.
+     *
+       curl -X POST $HOST:$PORT/recommendation -H "Content-Type: application/json" --data \
+       '{"productId":123,"recommendationId":456,"author":"jon","rate":5,"content":"yga, yga, yga"}'
+     *
+     * @param recommendation A JSON representation of the new recommendation
+     * @return A JSON representation of the newly created recommendation
+     */
+    @PostMapping(
+        value = "/recommendation",
+        consumes = "application/json",
+        produces = "application/json")
+    Recommendation createRecommendation(@RequestBody Recommendation recommendation);
+
+    /**
+     * Sample usage:
+     *
+       curl $HOST:$PORT/recommendation?productId=1
      *
      * @param productId ID of the product
      * @return the recommendations of the product
@@ -16,4 +33,14 @@ public interface RecommendationService {
     List<Recommendation> getRecommendations(
       @RequestParam(value = "productId") int productId
     );
+
+    /**
+     * Sample usage:
+     *
+       curl -X DELETE $HOST:$PORT/recommendation?productId=1
+     *
+     * @param productId ID of the product
+     */
+    @DeleteMapping(value = "/recommendation")
+    void deleteRecommendations(@RequestParam(value = "productId") int productId);
 }
