2de47244b94adf84f7bf287e9a82d465ab807341
lmarch <lmarch@yandex.ru>
2024-03-25 20:19:20 +0300

    Adding @PostMapping and @DeleteMapping in the review service interface.

--- api/src/main/java/com/lmarch/api/core/review/ReviewService.java
+++ api/src/main/java/com/lmarch/api/core/review/ReviewService.java
@@ -1,17 +1,44 @@
 package com.lmarch.api.core.review;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 public interface ReviewService {
+
     /**
-     * Sample usage: "curl $HOST:$PORT/review?productId=1".
+     * Sample usage:
+     *
+       curl -X POST $HOST:$PORT/review -H "Content-Type: application/json" --data \
+       '{"productId":123,"reviewId":456,"author":"me","subject":"yga, yga, yga","content":"yga, yga, yga"}'
+     *
+     * @param review A JSON representation of the new review
+     * @return A JSON representation of the newly created review
+     */
+    @PostMapping(
+        value = "/review",
+        consumes = "application/json",
+        produces = "application/json")
+    Review createReview(@RequestBody Review review);
+
+    /**
+     * Sample usage:
+     *
+       curl $HOST:$PORT/review?productId=1
      *
      * @param productId ID of the product
      * @return the reviews of the product
      */
     @GetMapping(value = "/review", produces = "application/json")
     List<Review> getReviews(@RequestParam(value = "productId") int productId);
+
+    /**
+     * Sample usage:
+     *
+       curl -X DELETE $HOST:$POST/review?productId=1
+     *
+     * @param productId ID of the product
+     */
+    @DeleteMapping(value = "/review")
+    void deleteReviews(@RequestParam(value = "productId") int productId);
 }
