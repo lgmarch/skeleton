479ae7c30838a80dff04ba7825cea6d50bd973ec
lmarch <lmarch@yandex.ru>
2024-03-26 17:26:39 +0300

    Adding calling the persistence layer from the service layer for the recommendation service.

--- microservices/recommendation-service/src/main/java/com/lmarch/microservices/core/recommendation/services/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/com/lmarch/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -3,26 +3,51 @@ package com.lmarch.microservices.core.recommendation.services;
 import com.lmarch.api.core.recommendation.Recommendation;
 import com.lmarch.api.core.recommendation.RecommendationService;
 import com.lmarch.api.exceptions.InvalidInputException;
+import com.lmarch.microservices.core.recommendation.persistence.RecommendationEntity;
+import com.lmarch.microservices.core.recommendation.persistence.RecommendationRepository;
 import com.lmarch.util.http.ServiceUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RecommendationServiceImpl implements RecommendationService {
     private static final Logger LOGGER = LoggerFactory.getLogger(RecommendationServiceImpl.class);
 
+    private final RecommendationRepository repository;
+    private final RecommendationMapper mapper;
     private final ServiceUtil serviceUtil;
 
     @Autowired
-    public RecommendationServiceImpl(ServiceUtil serviceUtil) {
+    public RecommendationServiceImpl(RecommendationRepository repository, RecommendationMapper mapper, ServiceUtil serviceUtil) {
+        this.repository = repository;
+        this.mapper = mapper;
         this.serviceUtil = serviceUtil;
+    }
+
+    @Override
+    public Recommendation createRecommendation(Recommendation body) {
+        try {
+            RecommendationEntity entity = mapper.apiToEntity(body);
+            RecommendationEntity newEntity = repository.save(entity);
+
+            LOGGER.debug(
+                "createRecommendation: created a body entity: {}/{}",
+                body.getProductId(),
+                body.getRecommendationId());
+
+            return mapper.entityToApi(newEntity);
+
+        } catch (DuplicateKeyException dke) {
+            throw new InvalidInputException(
+                "Duplicate key, Product Id: " + body.getProductId() + ", Recommendation Id:" + body.getRecommendationId());
+        }
     }
 
     @Override
@@ -31,18 +56,19 @@ public class RecommendationServiceImpl implements RecommendationService {
             throw new InvalidInputException("Invalid productId: " + productId);
         }
 
-        if (productId == 113) {
-            LOGGER.debug("No recommendations found for productId: {}", productId);
-            return new ArrayList<>();
-        }
+        List<RecommendationEntity> entityList = repository.findByProductId(productId);
+        List<Recommendation> list = mapper.entityListToApiList(entityList);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-        List<Recommendation> list = new ArrayList<>();
-        list.add(new Recommendation(productId, 1, "Author 1", 1, "Content 1", serviceUtil.getServiceAddress()));
-        list.add(new Recommendation(productId, 2, "Author 2", 2, "Content 2", serviceUtil.getServiceAddress()));
-        list.add(new Recommendation(productId, 3, "Author 3", 3, "Content 3", serviceUtil.getServiceAddress()));
-
-        LOGGER.debug("/recommendation response size: {}", list.size());
+        LOGGER.debug("getRecommendations: response size: {}", list.size());
 
         return list;
+    }
+
+    @Override
+    public void deleteRecommendations(int productId) {
+        LOGGER.debug("deleteRecommendations: tries to delete recommendations for the product with productId: {}", productId);
+
+        repository.deleteAll(repository.findByProductId(productId));
     }
 }
