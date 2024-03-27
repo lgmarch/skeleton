9c3be9e15f3da7e5057f3cdfc6eaca8792a81536
lmarch <lmarch@yandex.ru>
2024-03-27 03:13:26 +0300

    Updating the recommendation service tests.

--- microservices/recommendation-service/src/main/java/com/lmarch/microservices/core/recommendation/RecommendationServiceApplication.java
+++ microservices/recommendation-service/src/main/java/com/lmarch/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -1,15 +1,23 @@
 package com.lmarch.microservices.core.recommendation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.lmarch")
 public class RecommendationServiceApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RecommendationServiceApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(RecommendationServiceApplication.class, args);
-	}
+		ConfigurableApplicationContext ctx = SpringApplication.run(RecommendationServiceApplication.class, args);
 
+		String mongoDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+		String mongoDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+		LOGGER.info("Connected to MongoDb: " + mongoDbHost + ":" + mongoDbPort);
+	}
 }
